7cc8bfa1121570ef4d5135d25edd339788539597
dgw0620 <105223542+dgw0620@users.noreply.github.com>
2023-05-21 20:48:22 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 6일차

--- src/main/java/com/springboot/webservice_for_myself/config/auth/dto/SessionUser.java
+++ src/main/java/com/springboot/webservice_for_myself/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.springboot.webservice_for_myself.config.auth.dto;
 import com.springboot.webservice_for_myself.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
--- src/main/java/com/springboot/webservice_for_myself/web/IndexController.java
+++ src/main/java/com/springboot/webservice_for_myself/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.springboot.webservice_for_myself.web;
 
-import com.springboot.webservice_for_myself.domain.posts.Posts;
+import com.springboot.webservice_for_myself.config.auth.LoginUser;
+import com.springboot.webservice_for_myself.config.auth.dto.SessionUser;
 import com.springboot.webservice_for_myself.service.posts.PostsService;
 import com.springboot.webservice_for_myself.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+import java.nio.channels.SeekableByteChannel;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
